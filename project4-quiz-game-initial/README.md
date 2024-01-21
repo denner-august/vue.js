7dc018c62b58893afea5b423149ae0bb10db2e4c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-01-21 13:29:57 -0300

    config inicial

---
+++ project4-quiz-game-initial/README.md
@@ -0,0 +1,24 @@
+# project4-quiz-game-initial
+
+## Project setup
+```
+pnpm install
+```
+
+### Compiles and hot-reloads for development
+```
+pnpm run serve
+```
+
+### Compiles and minifies for production
+```
+pnpm run build
+```
+
+### Lints and fixes files
+```
+pnpm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ project4-quiz-game-initial/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="container">
+    <h1>Pergunta para responder</h1>
+
+    <label>true</label>
+    <input type="radio" name="options" value="true" />
+
+    <label>false</label>
+    <input type="radio" name="options" value="true" />
+
+    <button class="send" type="button">send</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  #container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    input,
+    label,
+    button {
+      margin: 0.5rem 0rem;
+      cursor: pointer;
+      font-size: 1.2rem;
+    }
+
+    input {
+      width: 2rem;
+      height: 2rem;
+    }
+
+    button {
+      background-color: blue;
+      color: white;
+
+      width: 10rem;
+      height: 3rem;
+
+      border: 0px;
+      border-radius: 0.5rem;
+    }
+  }
+}
+</style>
